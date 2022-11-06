9154598946f9e578485eb44bf69bf9214fa211a2
theGeekkit <sonofgondolin16@gmail.com>
2022-11-05 21:53:29 -0500

    11-5-22

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SecretComponent } from './secret/secret.component';
+import { LoginComponent } from './user/login/login.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'secret',
     component: SecretComponent,
-  }
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+
